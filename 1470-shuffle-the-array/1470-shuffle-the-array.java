a712411db6897884636a5d04a258615c0c4d1f23
Krishnakumar Naik <153983943+Krishnakumar-Naik@users.noreply.github.com>
2026-07-23 20:53:59 +0530

    Clean up shuffle method by removing comments
    Removed unused array declarations and commented code.

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,14 +1,15 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         int size=nums.length;
-        // int[] arr1=new int[n];
-        // int[] arr2=new int[n];
         int[] ans=new int[size];
 
         for(int i=0;i<n;i++){
             ans[2*i]=nums[i];
             ans[2*i+1]=nums[n+i];
         }        
+        
+        // int[] arr1=new int[n];
+        // int[] arr2=new int[n];
         // int k=0; int m=0;
         // for(int i=0;i<size;i++){
         //     if((i+1)%2==0){
